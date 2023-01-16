414f95bc38820110bd6173729dc99945c695cd1d
Maciej Marczyszyn <mmarc@pop-os.localdomain>
2023-01-16 19:51:21 +0100

    Zabij mnie

--- spark_nauka/main.py
+++ spark_nauka/main.py
@@ -1,11 +1,28 @@
 from pyspark.sql import SparkSession
+from pyspark.ml.image import ImageSchema
+from PIL import Image, ImageDraw
 import os
-os.environ['JAVA_HOME'] = 'D:/Program Files/jdk-19.0.1/'
-os.environ['HADOOP_HOME'] = 'D:/Program Files/spark-3.3.1-bin-hadoop3/'
-os.environ['PYTHONPATH'] = 'C:/Users/Denis/AppData/Local/Programs/Python/Python38/'
-os.environ['PYSPARK_PYTHON'] = 'C:/Users/Denis/AppData/Local/Programs/Python/Python38/python.exe'
-os.environ['PYSPARK_DRIVER_PYTHON'] = 'C:/Users/Denis/AppData/Local/Programs/Python/Python38/python.exe'
-spark=SparkSession.builder.master("spark://192.168.1.35:7077").appName("test").getOrCreate()
-rdd=spark.sparkContext.parallelize([1,2,3,4,5])
-wynik=rdd.count()
-print('WYNIK KURWA ',wynik)
+import sys
+spark=SparkSession.builder.master("local[").appName("test").getOrCreate()
+
+
+def procesing_images(rdf):
+    img = Image.frombytes(mode="RGB", data=bytes(rdf.image.data), size=[rdf.image.width,rdf.image.height])
+    img.convert("L")
+    box = (485,229,585,309)
+    croped_country = img.crop(box)
+    NW = croped_country.crop((0,0,50,40)) #NW
+    SW = croped_country.crop((0,40,50,80)) #SW
+    NE = croped_country.crop((50,0,100,40)) #NE
+    SE = croped_country.crop((50,40,100,80)) #SE
+    
+    return(rdf.image.origin,NW,0,SW,1,NE,2,SE,3)
+def print_rdf(rdf):
+    print(rdf)
+    
+df_images = spark.read.format("image").load("./Images")
+rdd = df_images.rdd
+yas = rdd.map(lambda f:procesing_images(f))
+
+
+print(type(yas))
